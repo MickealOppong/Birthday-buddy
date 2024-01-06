6bf82a0111c4c75a1b1253236c2f0ac20cfc7113
yaw oppong <opp.mic@gmail.com>
2024-01-06 10:12:01 +0100

    date change

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const buddies = [
   { id: 2, img: './imgs/img-2.jpeg', name: 'Anna Ama', dob: '2000-11-28' },
   { id: 3, img: './imgs/img-3.jpeg', name: 'Adjoa Mary', dob: '1978-03-20' },
   { id: 4, img: './imgs/img-4.jpeg', name: 'Susan Jones', dob: '2002-01-31' },
-  { id: 5, img: './imgs/img-5.jpeg', name: 'Esi Maame', dob: '1990-04-02' },
+  { id: 5, img: './imgs/img-5.jpeg', name: 'Esi Maame', dob: '1990-01-02' },
   { id: 6, img: './imgs/img-6.jpeg', name: 'Brad Jane', dob: '1967-06-18' },
   { id: 7, img: './imgs/img-7.jpeg', name: 'Lady Gaga', dob: '1999-07-31' },
   { id: 8, img: './imgs/img-8.jpeg', name: 'Monica Monik', dob: '1997-03-02' },
